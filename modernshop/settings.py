122c1ae837d9173d1dd72252e91cef63a1a6a956
nihas799 <nihasnavas81130@gmail.com>
2026-04-13 16:08:45 +0530

    updated final

--- modernshop/settings.py
+++ modernshop/settings.py
@@ -138,8 +138,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
