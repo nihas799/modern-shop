5aa4015f8f34b41302ed0cd0abfbfcf0d066a732
nihas799 <nihasnavas81130@gmail.com>
2026-04-14 11:53:54 +0530

    updated

--- modernshop/urls.py
+++ modernshop/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 
@@ -41,7 +39,4 @@ urlpatterns = [
          name='password_reset_complete'),
 
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
