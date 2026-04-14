db84c80a06b622410661cc68d6bb37cd35a2829a
nihas799 <nihasnavas81130@gmail.com>
2026-04-14 12:23:26 +0530

    cloudinary setup

--- modernshop/settings.py
+++ modernshop/settings.py
@@ -170,4 +170,10 @@ cloudinary.config(
     CLOUDINARY_API_SECRET= os.getenv('CLOUDINARY_API_SECRET'),
 )
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
