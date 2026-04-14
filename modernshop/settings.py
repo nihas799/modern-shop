8ad60cf03373e74b4813ee366c9f248139b8fdfa
nihas799 <nihasnavas81130@gmail.com>
2026-04-14 15:09:59 +0530

    storage

--- modernshop/settings.py
+++ modernshop/settings.py
@@ -174,6 +174,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
