18ee9345758d668e0fdd1529c3e36176b2a4b545
nihas799 <nihasnavas81130@gmail.com>
2026-04-14 15:03:40 +0530

    whitenoise

--- modernshop/settings.py
+++ modernshop/settings.py
@@ -132,11 +132,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-
-
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
