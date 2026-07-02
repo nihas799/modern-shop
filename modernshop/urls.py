aa90b46e047c359fab2d5ff287fab2fe8d1659cf
nihas799 <nihasnavas81130@gmail.com>
2026-07-02 21:16:15 +0530

    Reconnect Render database and fix static files

--- modernshop/urls.py
+++ modernshop/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -40,3 +42,5 @@ urlpatterns = [
 
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
